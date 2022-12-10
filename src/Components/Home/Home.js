a589b860ccaa9706612a1b1f6e9506ddc860e5b5
ajhridoy <ajhridoy461@gmail.com>
2022-12-11 04:34:45 +0600

    added contact form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Contact from './Contact me/Contact';
 import Projects from './Projects/Projects';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Projects></Projects>
+            <Contact></Contact>
         </div>
     );
 };
