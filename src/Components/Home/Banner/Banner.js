ffb84efe69a7bce187e81028847c0fbf13e977a5
ajhridoy <ajhridoy461@gmail.com>
2022-12-10 22:37:57 +0600

    added banner section

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import myImage from '../../../assests/Pastel Purple Mustard White Simple Professional Employment LinkedIn Profile Picture (1)-modified.png'
+import myImage from '../../../assests/Pastel Purple Mustard White Simple Professional Employment LinkedIn Profile Picture (2).png'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
     return (
-        <div>
             <section className="bg-gray-800 text-gray-100">
 	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className='text-2xl font-bold uppercase'>Hello, this is Abu Zubayer</h1>
-            <h2>I am a Mern Stack Developer</h2>
+		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-lg xl:max-w-lg lg:text-left">
+            <h1 className='text-2xl md:text-4xl font-bold uppercase text-cyan-400'>HI! <span>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['this is Abu Zubayer']}
+            loop={false}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={80}
+            delaySpeed={1500}
+          />
+        </span></h1>
+            <p className='md:text-lg font-mono mt-3 text-amber-500'>A front-end focused developer Who has deep knowledge about Mern Stack Development. I have worked on many projects using JavaScript, React.JS, Node.JS and MongoDB.</p>
+            <a href='https://drive.google.com/file/d/1Q0MRV1z9hRgHZnW-7hrpdk7nY92MUaFU/view?usp=share_link' target='_blank' className='px-8 py-3 font-semibold w-1/2 mt-6 rounded-full bg-yellow-400 hover:bg-yellow-600 text-black text-center mx-auto'>See My Resume</a>
 		</div>
 		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
 			<img src={myImage} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
 		</div>
 	</div>
 </section>
-        </div>
     );
 };
 
